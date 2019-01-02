a32d77cc68360eaafc02da0249852df890e769a0
Huynh Tuan Anh <huynhtuananh21895@gmail.com>
2019-01-02 23:35:57 +0700

    updated st

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/<category>')
+def category(category):
+    return render_template('productgrid.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
