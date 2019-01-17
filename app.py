57c8c12b558164a8e215e14f51aae1ef8a7ae76a
hta218 <huynhtuananh21895@gmail.com>
2019-01-17 11:39:11 +0700

    Added contact page

--- app.py
+++ app.py
@@ -89,5 +89,9 @@ def checkout():
 
         return 'done'
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
